cb55d3ad9414a5103922e76d94cdc4e5dce491c8
Civitasv <2309780282@qq.com>
2021-08-27 14:32:54 +0800

    floyd algorithm added

---
+++ Crash Course on Python/algorithm/Floyd_Hare_Tortoise.py
@@ -0,0 +1,22 @@
+def findDuplicate(arr):
+    tortoise = arr[0]
+    hare = arr[0]
+
+    while True:
+        tortoise = arr[tortoise]
+        hare = arr[arr[hare]]
+
+        if tortoise == hare:
+            break
+
+    ptr1 = arr[0]
+    ptr2 = tortoise
+
+    while ptr1 != ptr2:
+        ptr1 = arr[ptr1]
+        ptr2 = arr[ptr2]
+
+    return ptr1
+
+
+print(findDuplicate([1, 2, 3, 3, 4, 5]))
