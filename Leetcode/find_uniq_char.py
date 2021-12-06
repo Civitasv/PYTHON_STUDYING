7fa231b9951843be72e3c7b88094ae7a2d5fb923
Civitasv <2309780282@qq.com>
2021-12-06 11:51:38 +0800

    剑指offer 第五天 finished

---
+++ Leetcode/find_uniq_char.py
@@ -0,0 +1,15 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> str:
+        set = []
+        alphabets = [0 for _ in range(26)]
+        for c in s:
+            alphabets[ord(c)-97] += 1
+
+        for c in s:
+            if alphabets[ord(c)-97] == 1:
+                return c
+        return " "
+
+
+s = Solution()
+print(s.firstUniqChar("aadadaad"))
