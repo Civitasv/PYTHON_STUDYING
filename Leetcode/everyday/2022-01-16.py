2ee5b0b46ddf9524114cc4f54a2924caf1682c07
Civitasv <2309780282@qq.com>
2022-01-16 22:37:59 +0800

    20220116 finished

--- Leetcode/everyday/2022-01-16.py
+++ Leetcode/everyday/2022-01-16.py
@@ -1,5 +1,5 @@
 from typing import Optional
-
+import random
 # Definition for singly-linked list.
 
 
@@ -12,9 +12,21 @@ class ListNode:
 class Solution:
 
     def __init__(self, head: Optional[ListNode]):
+        self.head = head
 
     def getRandom(self) -> int:
+        p = self.head
+        count, res = 0, 0
+        while p:
+            count += 1
+            rand = random.randint(count)+1
+            if rand == count:
+                res = p.val
+            p = p.next
 
-        # Your Solution object will be instantiated and called as such:
-        # obj = Solution(head)
-        # param_1 = obj.getRandom()
+        return res
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(head)
+# param_1 = obj.getRandom()
